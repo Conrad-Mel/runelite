8d62d3cb6f1f9472e1f0caa6d340d99af04e0408
Dillon Kesto <dkesto@umich.edu>
2019-02-24 12:08:04 -0500

    fixed one instances of bug where highlighting was still occuring even though config was off

--- runelite-client/src/main/java/net/runelite/client/plugins/inventorysetups/InventorySetupPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/inventorysetups/InventorySetupPlugin.java
@@ -208,10 +208,7 @@ public class InventorySetupPlugin extends Plugin
 			// only allow highlighting if the config is enabled and the player is logged in
 			highlightDifference = config.getHighlightDifferences() && client.getGameState() == GameState.LOGGED_IN;
 			final String setupName = panel.getSelectedInventorySetup();
-			if (highlightDifference && !setupName.isEmpty())
-			{
-				panel.setCurrentInventorySetup(setupName);
-			}
+			panel.setCurrentInventorySetup(setupName);
 		}
 	}
 
@@ -296,10 +293,7 @@ public class InventorySetupPlugin extends Plugin
 			case LOGIN_SCREEN:
 				highlightDifference = false;
 				final String setupName = panel.getSelectedInventorySetup();
-				if (!setupName.isEmpty())
-				{
-					panel.setCurrentInventorySetup(setupName);
-				}
+				panel.setCurrentInventorySetup(setupName);
 				break;
 
 			// set highlighting
